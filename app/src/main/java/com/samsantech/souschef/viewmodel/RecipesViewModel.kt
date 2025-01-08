5a0d042448b659042712b6b2efc2952dd852cd9e
Einha Alliah Muday <132117926+einhaalliahgmuday@users.noreply.github.com>
2025-01-09 03:42:07 +0800

    implemented filter state for search queries, particularly for recipe categories

--- app/src/main/java/com/samsantech/souschef/viewmodel/RecipesViewModel.kt
+++ app/src/main/java/com/samsantech/souschef/viewmodel/RecipesViewModel.kt
@@ -6,66 +6,106 @@ import com.algolia.instantsearch.android.paging3.searchbox.connectPaginator
 import com.algolia.instantsearch.compose.loading.LoadingState
 import com.algolia.instantsearch.compose.searchbox.SearchBoxState
 import com.algolia.instantsearch.core.connection.ConnectionHandler
+import com.algolia.instantsearch.filter.state.FilterGroupID
+import com.algolia.instantsearch.filter.state.FilterOperator
+import com.algolia.instantsearch.filter.state.FilterState
+import com.algolia.instantsearch.filter.state.add
+import com.algolia.instantsearch.filter.state.remove
 import com.algolia.instantsearch.loading.LoadingConnector
 import com.algolia.instantsearch.loading.connectView
 import com.algolia.instantsearch.searchbox.SearchBoxConnector
 import com.algolia.instantsearch.searchbox.connectView
+import com.algolia.instantsearch.searcher.connectFilterState
 import com.algolia.instantsearch.searcher.hits.HitsSearcher
 import com.algolia.instantsearch.searcher.hits.SearchForQuery
 import com.algolia.search.model.APIKey
 import com.algolia.search.model.ApplicationID
+import com.algolia.search.model.Attribute
 import com.algolia.search.model.IndexName
-import com.algolia.search.model.search.Query
+import com.algolia.search.model.filter.Filter
 import com.samsantech.souschef.data.Recipe
 import com.samsantech.souschef.data.SearchRecipe
 import kotlinx.coroutines.flow.MutableStateFlow
 
 class RecipesViewModel: ViewModel() {
     val displayRecipe = MutableStateFlow(Recipe())
+    private var category: String = ""
+    private val categoriesId = FilterGroupID("categoriesId", FilterOperator.Or)
 
-    private val query: Query = Query(
-        query = "ayna",
-        filters = "audience:\"Public\""
-    )
-
-    private val searcher = HitsSearcher(
+    private var searcher = HitsSearcher(
         applicationID = ApplicationID("VP98Z77775"),
         apiKey = APIKey("f5fad5c803ab4df0ea8c02f45496c71c"),
         indexName = IndexName("souschef-samsantech"),
         triggerSearchFor = SearchForQuery.lengthAtLeast(1),
-        query = query,
-        isDisjunctiveFacetingEnabled = false
+        isDisjunctiveFacetingEnabled = false,
     )
+    private val filterState = FilterState()
 
-    private val searchBoxConnector = SearchBoxConnector(searcher)
+    private var searchBoxConnector = SearchBoxConnector(searcher)
     val searchBoxState = SearchBoxState()
-    val hitsPaginator = Paginator(searcher) {
-        println(it)
+    var hitsPaginator = Paginator(searcher) {
+        if (category != "") {
+            filterState.notify {
+                remove(
+                    categoriesId,
+                    setOf(
+                        Filter.Facet(Attribute("categories"), category)
+                    )
+                )
+            }
+
+            category = ""
+        }
+
         it.deserialize(SearchRecipe.serializer())
     }
     val loadingState = LoadingState()
     private val loadingConnector = LoadingConnector(searcher)
 
-//    private val filterState = FilterState()
-//    private val audience = Attribute("audience")
-//    private val filterListConnector = FilterListConnector.Facet(
-//        filters = listOf(Filter.Facet(audience, "Only me")),
-//        filterState = filterState,
-//        selectionMode = SelectionMode.Single
-//    )
-//    val filterListState = FilterListState<Filter.Facet>()
+    private val connections = ConnectionHandler(
+        searchBoxConnector,
+        loadingConnector
+    )
 
-    private val connections = ConnectionHandler(searchBoxConnector, loadingConnector)
-    // , searcher.connectFilterState(filterState)
     init {
+        connections += searcher.connectFilterState(filterState)
         connections += searchBoxConnector.connectView(searchBoxState)
         connections += searchBoxConnector.connectPaginator(hitsPaginator)
         connections += loadingConnector.connectView(loadingState)
-//        connections += filterListConnector.connectView(filterListState)
+
+        val audienceId = FilterGroupID("audienceId", FilterOperator.Or)
+        filterState.notify {
+            add(
+                audienceId,
+                setOf(
+                    Filter.Facet(Attribute("audience"), "Public")
+                )
+            )
+        }
     }
 
     override fun onCleared() {
         super.onCleared()
+        cancelSearch()
+    }
+
+    fun cancelSearch() {
         searcher.cancel()
+    }
+
+    fun searchCategory(category: String) {
+        this.category = category
+
+        filterState.notify {
+            add(
+                categoriesId,
+                setOf(
+                    Filter.Facet(Attribute("categories"), category)
+                )
+            )
+        }
+
+        searchBoxState.setText(" ")
+        loadingState.setIsLoading(true)
     }
 }
